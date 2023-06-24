1a6c056efb4de8a7127dc52fbbaaa7ed99cc1a26
kminvita <vita_mink@daum.net>
2023-06-25 00:09:52 +0900

    Feat/reset.css사용, 절대경로 만드는 방법(jsconfig.json)

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
 import React, { useState } from "react";
-import Counter from "./components/Counter";
 
 function App() {
-  const [counter, setCounter] = useState(1);
-  const [isOpen, setIsOpen] = useState(false);
   return (
-  <>
-    <Counter counter={counter} setCounter={setCounter} />
-  </>
+  <></>
   );
 }
 
