2e2d93a4897907b11b43fc2d6f60f2a7a20e66f0
kminvita <vita_mink@daum.net>
2023-06-25 02:09:06 +0900

    Add/map이용해서 더미데이터 보여주기, dayjs, classnames로 css적용

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,9 +1,34 @@
 import './main.css';
 import { CommonModal } from '../../components';
 import { useState } from 'react';
+import dayjs from 'dayjs';
+import cx from 'classnames';
+
+const DUMMY_TODOLIST = [
+    {
+        id: 1,
+        title: '할 일 1입니다.',
+        content: '내용 1입니다.',
+        createdAt: dayjs().format('YYYY.MM.DD HH:mm:ss'),
+        updatedAt: dayjs().format('YYYY.MM.DD HH:mm:ss'),
+        isComplete: true,
+    },
+    {
+        id: 2,
+        title: '할 일 2입니다.',
+        content: '내용 2입니다.',
+        createdAt: dayjs().format('YYYY.MM.DD HH:mm:ss'),
+        updatedAt: dayjs().format('YYYY.MM.DD HH:mm:ss'),
+        isComplete: false,
+    }
+];
+
+dayjs.locale('ko');
 
 function MainPage() {
     const [isOpen, setIsOpen] = useState(false);
+
+    const [todoList, setTodolist] = useState(DUMMY_TODOLIST);
 
     return (
         <>
@@ -11,11 +36,43 @@ function MainPage() {
             <main>
                 <h1>our React Todolist</h1>
                 <div className='Topnavbar'>
-                    <time>TODAY: 2023.06.25</time>
+                    <time>TODAY: {dayjs().format('YYYY.MM.DD')}</time>
                     <button type='button' className='addbutton'>추가</button>
                 </div>
-                {/* 리스트가 없을 경우에는 추가해주세요 라는 텍스트 추가할것임 */}
                 <section className='todolist'>
+                    {todoList.map((item) => {
+                        return (
+                                <article key={item.id} className={cx("todoitem", { complete: item.isComplete })}>
+                                    <div>
+                                        <p className='todotitle'>{item.title}</p>
+                                        <time className='createdate'>생성날짜 {item.createdAt}</time>
+                                    </div>
+                                    <div>
+                                        <button 
+                                            type='button'
+                                            className='completebutton'
+                                        >
+                                            {item.isComplete ? '완료해제' : '완료'}
+                                        </button>
+                                        <button 
+                                            type='button' 
+                                            className='editbutton'
+                                        >
+                                            수정
+                                        </button>
+                                        <button 
+                                            type='button' 
+                                            className='deletebutton'
+                                        >
+                                            삭제
+                                        </button>
+                                    </div>
+                                </article>
+                        )
+                    })}
+                </section>
+                {/* 리스트가 없을 경우에는 추가해주세요 라는 텍스트 추가할것임 */}
+                {/* <section className='todolist'>
                     <article className='todoitem'>
                         <div>
                             <p className='todotitle'>제목입니다</p>
@@ -26,7 +83,7 @@ function MainPage() {
                             <button type='button' className='deletebutton'>삭제</button>
                         </div>
                     </article>
-                </section>
+                </section> */}
             </main>
             <CommonModal isOpen={isOpen} />
         </>
